438e71b28594f926dcefb38f366ba31d33834ff8
cso1999888 <66512887+cso1999888@users.noreply.github.com>
2020-07-04 20:54:31 +0800

    20200704晚

---
+++ 2D RUN/Assets/Script/Test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("press space to get random number (100-500)");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //取得 100 - 500 的隨機值
+        if (Input.GetKeyDown("space"))
+        {
+            print(Random.Range(100, 501));
+            //(min is inclusive, while max is exclusive)
+        }
+    }
+}
--- 2D RUN/Assets/Script/player.cs
+++ 2D RUN/Assets/Script/player.cs
@@ -2,11 +2,11 @@
 
 public class player : MonoBehaviour
 {
+    #region Data type 
     //[Header("string")]
     //[Tooltip("string")]
     //[Range(float min,float max)]
-    #region Data type 
-    [Header("速度"),Tooltip("血量"),Range(0,100)]
+    [Header("速度"), Tooltip("血量"), Range(0, 100)]
     public float speed = 1f;
     [Header("血量")]
     public float HP;
@@ -21,5 +21,58 @@ public class player : MonoBehaviour
     public AudioClip SoundPoint;
     [Header("死亡")]
     public bool dead;
+    #endregion
+
+    #region Methods
+    /// <summary>
+    /// Jump: 跳躍動畫、跳躍音效、角色往上
+    /// </summary>
+    private void Jump()
+    {
+        print("跳躍");
+    }
+
+    /// <summary>
+    /// Slide: 滑行動畫、滑行音效
+    /// </summary>
+    private void Slide()
+    {
+        print("滑行");
+    }
+
+    /// <summary>
+    /// Hurt: 受傷動畫、受傷音效、扣血量、更新介面（敵人消失）
+    /// </summary>
+    private void Hurt()
+    {
+
+    }
+
+    /// <summary>
+    /// GetPoint: 得分音效、更新得分、更新介面（草莓消失）
+    /// </summary>
+    private void GetPoint()
+    {
+
+    }
+
+    /// <summary>
+    /// Die: 死亡動畫、死亡音效
+    /// </summary>
+    private void Die()
+    {
+
+    }
+    #endregion
+
+    #region Event
+    private void Start() 
+    {
+
+    }
+    private void Update()
+    {
+
+    }
     #endregion
 }
